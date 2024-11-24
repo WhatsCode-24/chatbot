839d88c74cde148bec5977c611209f75dd4c4057
Ryan Alves <Ryan.queen246@gmail.com>
2024-11-24 15:48:11 -0300

    adicionando correção de acentuação

--- app.py
+++ app.py
@@ -4,9 +4,18 @@ from chatterbot.trainers import ListTrainer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk
+from nltk.corpus import mac_morpho
+from nltk.metrics import edit_distance
+from collections import Counter
+
 
 nltk.download('punkt_tab')
 nltk.download('stopwords')  # Para obter stopwords
+nltk.download('mac_morpho')
+
+palavras = [palavra.lower() for palavra in mac_morpho.words()]
+vocabulario = set(palavras)
+frequencias = Counter(palavras)
 
 app = Flask(__name__)
 
@@ -34,132 +43,128 @@ if __name__ == "__main__":
    ################# TREINO #################
 
    list_training = [
-      [
-         [
-               "oi",
-               "ola",
-               "oie",
-               "oin",
-               "salve",
-               "oizinho",
-         ],
-         "Ola! Como eu poderia ser util?"
-      ],
-      [
-         [
-               "onde posso achar a pagina de visualizar usuarios",
-               "onde esta a pagina de visualizar usuarios",
-               "como posso visualizar usuarios",
-         ],
-         "Voce pode encontrar a pagina clicando no modal que aparece quando ao clicar na pagina de 'Gerenciar usuarios'."
-      ],
-      [
-         [
-               "listar areas",
-               "listar area",
-               "listar todas areas",
-         ],
-         "Voce pode listar as areas cadastradas clicando no botao 'Gerenciar Areas' na barra lateral."
-      ],
-      [
-         [
-               "onde vejo o acesso as areas",
-               "onde posso ver o acesso as areas",
-               "qual lugar posso verificar o acesso as areas",
-               "onde poderia achar o acesso das areas",
-         ],
-         "Voce pode entrar na pagina de 'Ultimos Acessos' para melhor visualizacao dos acessos."
-      ],
-      [
-         [
-            "Como vejo o horario de funcionamento da empresa?",
-            "Qual o horario de atendimento da empresa?",
+    [
+        [
+            "oi",
+            "olá",
+        ],
+        "Olá! Como eu poderia ser útil?"
+    ],
+    [
+        [
+            "onde posso achar a página de visualizar usuários",
+            "onde está a página de visualizar usuários",
+            "como posso visualizar usuários",
+        ],
+        "Você pode encontrar a página clicando no modal que aparece ao clicar na página de 'Gerenciar usuários'."
+    ],
+    [
+        [
+            "listar áreas",
+            "listar área",
+            "listar todas áreas",
+        ],
+        "Você pode listar as áreas cadastradas clicando no botão 'Gerenciar Áreas' na barra lateral."
+    ],
+    [
+        [
+            "onde vejo o acesso às áreas",
+            "onde posso ver o acesso às áreas",
+            "qual lugar posso verificar o acesso às áreas",
+            "onde poderia achar o acesso das áreas",
+        ],
+        "Você pode entrar na página de 'Últimos Acessos' para melhor visualização dos acessos."
+    ],
+    [
+        [
+            "Como vejo o horário de funcionamento da empresa?",
+            "Qual o horário de atendimento da empresa?",
             "A que horas a empresa abre e fecha?"
-         ],
-         "Voce pode ver o horario de funcionamento acessando o menu 'Gerenciar Empresas'. La, voce encontrara todas empresas listadas com os respectivos horarios de abertura e fechamento."
-      ],
-      [
-         [
-            "Como vejo a lista de usuarios de uma empresa?",
-            "Quais usuarios estao cadastrados em uma empresa?",
-            "Como saber quem sao os usuarios de uma empresa especifica?"
-         ],
-         "Para ver a lista de usuarios de uma empresa, va em 'Gerenciar Empresas', la, voce encontrara todos os usuarios listados"
-      ],
-      [
-         [
-            "Quais cameras estao instaladas em um comodo?",
-            "Como vejo as cameras disponiveis em um comodo especifico?",
-            "Onde encontro as cameras de um determinado local?"
-         ],
-         "Va ate 'Gerenciar Areas' e selecione o comodo desejado para visualizar as imagens das cameras."
-      ],
-      [
-         [
-            "Como vejo as portas de um comodo?",
-            "Quais portas estao instaladas em um determinado comodo?",
-            "Onde posso ver as portas de um comodo especifico?"
-         ],
-         "Para ver as portas de um comodo, acesse 'Gerenciar Areas'. La estarao listadas as portas instaladas."
-      ],
-      [
-         [
-            "Onde vejo os detalhes dos acessos em um comodo?",
-            "Como posso acessar o historico de acessos de um comodo?",
-            "Quais informacoes estao disponiveis sobre acessos em um comodo?"
-         ],
-         "No menu, va ate 'Gerenciar Acessos', la estarao listados os detalhes dos acessos"
-      ],
-      [
-         [
-            "Como saber o nivel de acesso de um usuario?",
-            "Qual o nivel de permissao de um usuario?",
-            "Como posso ver as permissoes de um usuario especifico?"
-         ],
-         "No menu 'Gerenciar Usuarios', selecione o usuario para visualizar o nivel de acesso e suas permissoes no sistema."
-      ],
-      [
-         [
-            "Como vejo os dados de um usuario?",
-            "Onde encontro as informacoes de contato de um usuario?",
-            "Como acessar o perfil de um usuario especifico?"
-         ],
-         "Acesse 'Gerenciar Usuarios' no menu, la estarao listados os dados de contato e outras informacoes de usuario."
-      ],
-      [
-         [
-            "Como posso ver as informacoes da empresa?",
-            "Quais sao os detalhes de uma empresa cadastrada?",
-            "Onde vejo o endereco e contato de uma empresa?"
-         ],
-         "No menu va em 'Gerenciar Empresas', la existe uma lista com as informacoes de todas empresas cadastradas como endereco, telefone e horarios de funcionamento."
-      ],
-      [
-         [
-            "Como vejo os comodos de uma empresa?",
-            "Onde posso encontrar informacoes sobre os comodos de uma empresa?",
-            "Como acessar a lista de comodos de uma empresa?"
-         ],
-         "No menu va em 'Gerenciar Areas', la existe uma lista com todos os comodos cadastrados."
-      ],
-      [
-         [
+        ],
+        "Você pode ver o horário de funcionamento acessando o menu 'Gerenciar Empresas'. Lá, você encontrará todas as empresas listadas com os respectivos horários de abertura e fechamento."
+    ],
+    [
+        [
+            "Como vejo a lista de usuários de uma empresa?",
+            "Quais usuários estão cadastrados em uma empresa?",
+            "Como saber quem são os usuários de uma empresa específica?"
+        ],
+        "Para ver a lista de usuários de uma empresa, vá em 'Gerenciar Empresas'. Lá, você encontrará todos os usuários listados."
+    ],
+    [
+        [
+            "Quais câmeras estão instaladas em um cômodo?",
+            "Como vejo as câmeras disponíveis em um cômodo específico?",
+            "Onde encontro as câmeras de um determinado local?"
+        ],
+        "Vá até 'Gerenciar Áreas' e selecione o cômodo desejado para visualizar as imagens das câmeras."
+    ],
+    [
+        [
+            "Como vejo as portas de um cômodo?",
+            "Quais portas estão instaladas em um determinado cômodo?",
+            "Onde posso ver as portas de um cômodo específico?"
+        ],
+        "Para ver as portas de um cômodo, acesse 'Gerenciar Áreas'. Lá estarão listadas as portas instaladas."
+    ],
+    [
+        [
+            "Onde vejo os detalhes dos acessos em um cômodo?",
+            "Como posso acessar o histórico de acessos de um cômodo?",
+            "Quais informações estão disponíveis sobre acessos em um cômodo?"
+        ],
+        "No menu, vá até 'Gerenciar Acessos'. Lá estarão listados os detalhes dos acessos."
+    ],
+    [
+        [
+            "Como saber o nível de acesso de um usuário?",
+            "Qual o nível de permissão de um usuário?",
+            "Como posso ver as permissões de um usuário específico?"
+        ],
+        "No menu 'Gerenciar Usuários', selecione o usuário para visualizar o nível de acesso e suas permissões no sistema."
+    ],
+    [
+        [
+            "Como vejo os dados de um usuário?",
+            "Onde encontro as informações de contato de um usuário?",
+            "Como acessar o perfil de um usuário específico?"
+        ],
+        "Acesse 'Gerenciar Usuários' no menu. Lá estarão listados os dados de contato e outras informações do usuário."
+    ],
+    [
+        [
+            "Como posso ver as informações da empresa?",
+            "Quais são os detalhes de uma empresa cadastrada?",
+            "Onde vejo o endereço e contato de uma empresa?"
+        ],
+        "No menu, vá em 'Gerenciar Empresas'. Lá existe uma lista com as informações de todas as empresas cadastradas, como endereço, telefone e horários de funcionamento."
+    ],
+    [
+        [
+            "Como vejo os cômodos de uma empresa?",
+            "Onde posso encontrar informações sobre os cômodos de uma empresa?",
+            "Como acessar a lista de cômodos de uma empresa?"
+        ],
+        "No menu, vá em 'Gerenciar Áreas'. Lá existe uma lista com todos os cômodos cadastrados."
+    ],
+    [
+        [
             "Quem acessou uma porta recentemente?",
-            "Como vejo o historico de acessos de uma porta?",
-            "Quais foram os ultimos acessos registrados em uma porta?"
-         ],
-         "Para ver o historico de acessos de uma porta, va em 'Gerenciar Acessos', la existe uma lista com todos os registros de acessos."
-      ],
-      [
-         [
-            "Quem pode acessar um comodo especifico?",
-            "Quais usuarios tem permissao para entrar em um determinado comodo?",
-            "Como vejo as permissoes de acesso de um comodo?"
-         ],
-         "Va ate 'Gerenciar Acessos' para ver quem tem permissao de acesso em cada comodo. La voce podera configurar e visualizar as permissoes dos usuarios."
-      ],
-      [
-         [
+            "Como vejo o histórico de acessos de uma porta?",
+            "Quais foram os últimos acessos registrados em uma porta?"
+        ],
+        "Para ver o histórico de acessos de uma porta, vá em 'Gerenciar Acessos'. Lá existe uma lista com todos os registros de acessos."
+    ],
+    [
+        [
+            "Quem pode acessar um cômodo específico?",
+            "Quais usuários têm permissão para entrar em um determinado cômodo?",
+            "Como vejo as permissões de acesso de um cômodo?"
+        ],
+        "Vá até 'Gerenciar Acessos' para ver quem tem permissão de acesso em cada cômodo. Lá você poderá configurar e visualizar as permissões dos usuários."
+    ],
+    [
+        [
             "Obrigado",
             "Valeu",
             "Obrigada",
@@ -167,11 +172,12 @@ if __name__ == "__main__":
             "Agradecido",
             "Agradecida",
             "Obrigado pela ajuda",
-            "Valeu pela assistencia"
-         ],
-         "De nada! Estou aqui para ajudar. Se precisar de mais alguma coisa, e so chamar!"
-      ]
-   ]
+            "Valeu pela assistência"
+        ],
+        "De nada! Estou aqui para ajudar. Se precisar de mais alguma coisa, é só chamar!"
+    ]
+]
+
 
    
 
@@ -183,7 +189,18 @@ if __name__ == "__main__":
 
 ################# ROTAS #################
 # Obter as stopwords em português
-stop_words = set(stopwords.words('portuguese'))
+print(type(stopwords.words('portuguese')))
+stop_words = stopwords.words('portuguese') + ['um', 'uma','de','da', 'do', 'o', 'a']
+stop_words = set(stop_words)
+
+def corrigir_ou_validar(palavra, vocabulario):
+
+    if palavra in vocabulario:
+        return palavra  # Palavra já está correta
+    else:
+        # Encontrar a palavra mais próxima com base na distância de edição
+        sugestao = min(vocabulario, key=lambda v: edit_distance(palavra, v))
+        return sugestao
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot_answer():
@@ -192,10 +209,13 @@ def chatbot_answer():
 
     # Tokenizar a entrada do usuário
     tokens = word_tokenize(query, language='portuguese')
-
+    listwords = []
+    for palavra in tokens:
+      resultado = corrigir_ou_validar(palavra.lower(), vocabulario)
+      listwords.append(resultado)
+      
     # Remover as stopwords
-    palavras_sem_stopwords = [word for word in tokens if word.lower() not in stop_words]
-
+    palavras_sem_stopwords = [word for word in listwords if word.lower() not in stop_words]
     # Obter a resposta do chatbot
     response = str(chatbot.get_response(" ".join(palavras_sem_stopwords)))
     
